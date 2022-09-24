030bcb09e2e78cbce2977baff2b2b5f3bdd30f09
riquew <hwindlin@gmail.com>
2022-09-24 16:13:25 -0300

    Adciona rota e params para as categorias em restaurantes

--- src/pages/Categorias/CategoriasPage.jsx
+++ src/pages/Categorias/CategoriasPage.jsx
@@ -19,6 +19,7 @@ function CategoriasPage() {
 		const result = await getAllCategorias();
 		setCategorias(result.data);
 		setLoading(false);
+		console.log(result.data)
 	};
 
 	useEffect(() => {
@@ -40,7 +41,7 @@ function CategoriasPage() {
 					<Grid item xs={4} key={categoria.id}>
 						<div
 							className="containerCategorias"
-							onClick={() => navigate(`/restaurantes/${categoria.id}`, {id:categoria.id})}
+							onClick={() => navigate(`/restaurantes/${categoria.ID}`, { state:categoria })}
 						>
 
 							<img
@@ -49,17 +50,11 @@ function CategoriasPage() {
 								alt={categoria.name}
 								className="imgCategory"
 							/>
-<<<<<<< HEAD
-							
-							<Typography className="textNames">{categoria.name}</Typography>
-							<Typography className="textNames">({categoria.total})</Typography>
-=======
 							<div className="containerTitulo">
 								<Typography className="textNames">{categoria.name}</Typography>
 								<Typography className="textNames">({categoria.total})</Typography>
 							</div>
 
->>>>>>> 5770b0f3e02dc886aa8ce8bd65e3a48db978303a
 						</div>
 					</Grid>
 				))}
--- src/pages/Restaurantes/RestaurantesPage.jsx
+++ src/pages/Restaurantes/RestaurantesPage.jsx
@@ -1,6 +1,6 @@
 import { Container, Typography, CircularProgress } from "@material-ui/core";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { getRestaurantes } from "../../services/restaurantes.service";
 import "./style.css";
 
@@ -10,18 +10,20 @@ function RestaurantesPage() {
   const [restaurantesNoPreco, setRestaurantesNoPreco] = useState([]);
   const [restaurantesCaro, setRestaurantesCaro] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { id } = useParams();
+  const { state } = useLocation();
 
   useEffect(() => {
-      console.log(id);
-      getRestaurantes().then((response) => {
-      setNomeCategoria(response.categoria)
+      console.log(state)
+      getRestaurantes(state.ID).then((response) => {
+      console.log(response);
+      setNomeCategoria(state.name)
       setRestaurantesBaratinho(response.baratinho);
       setRestaurantesNoPreco(response.no_preco);
       setRestaurantesCaro(response.caro);
       setLoading(false);
     })
-  }, [id]);
+    
+  }, [state]);
 
   return (
     <Container className="restaurantes">
@@ -40,9 +42,44 @@ function RestaurantesPage() {
       </div>
       {restaurantesBaratinho?.map(restaurante => (
         <div key={restaurante.id}>
+          <img src={restaurante.imagem} alt={restaurante.nome}/>
           {restaurante.nome}
+          {restaurante.distancia}
+          {restaurante.nota}
+          {restaurante.tempo_medio}
         </div>
       ))}
+
+  <div className="sub-header">
+        <Typography variant="body1" color="primary">
+          No Preço <span>(</span>$ $ $ <span> $ $)</span>
+        </Typography>
+      </div>
+      {restaurantesNoPreco?.map(restaurante => (
+        <div key={restaurante.id}>
+          <img src={restaurante.imagem} alt={restaurante.nome}/>
+          {restaurante.nome}
+          {restaurante.distancia}
+          {restaurante.nota}
+          {restaurante.tempo_medio}
+        </div>
+      ))}
+
+  <div className="sub-header">
+        <Typography variant="body1" color="primary">
+          Caro mas vale a pena <span>(</span>$ $ $ $ $<span>)</span>
+        </Typography>
+      </div>
+      {restaurantesCaro?.map(restaurante => (
+        <div key={restaurante.id}>
+          <img src={restaurante.imagem} alt={restaurante.nome}/>
+          {restaurante.nome}
+          {restaurante.distancia}
+          {restaurante.nota}
+          {restaurante.tempo_medio}
+        </div>
+      ))}
+      
     </Container>
   )
 }
