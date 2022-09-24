4595fc023e25f47b15ca6718f81f70598d617061
elizalap <elizapaoliello@outlook.com>
2022-09-24 17:07:37 -0300

    Estilo da página de restaurantes

--- src/pages/Restaurantes/RestaurantesPage.jsx
+++ src/pages/Restaurantes/RestaurantesPage.jsx
@@ -13,8 +13,8 @@ function RestaurantesPage() {
   const { state } = useLocation();
 
   useEffect(() => {
-      console.log(state)
-      getRestaurantes(state.ID).then((response) => {
+    console.log(state)
+    getRestaurantes(state.ID).then((response) => {
       console.log(response);
       setNomeCategoria(state.name)
       setRestaurantesBaratinho(response.baratinho);
@@ -22,7 +22,7 @@ function RestaurantesPage() {
       setRestaurantesCaro(response.caro);
       setLoading(false);
     })
-    
+
   }, [state]);
 
   return (
@@ -40,46 +40,59 @@ function RestaurantesPage() {
           Baratinho <span>(</span>$ <span>$ $ $ $)</span>
         </Typography>
       </div>
+
       {restaurantesBaratinho?.map(restaurante => (
-        <div key={restaurante.id}>
-          <img src={restaurante.imagem} alt={restaurante.nome}/>
-          {restaurante.nome}
-          {restaurante.distancia}
-          {restaurante.nota}
-          {restaurante.tempo_medio}
+        <div >
+          <div className="restaurantesContainer" key={restaurante.id}>
+            <img src={restaurante.imagem} alt={restaurante.nome} />
+            <div className="descricaoContainer">
+              <h3 className="titulo">{restaurante.nome}</h3>
+              <div className="tempo">{restaurante.distancia}
+                {restaurante.nota}
+                {restaurante.tempo_medio}
+              </div>
+            </div>
+          </div>
         </div>
       ))}
 
-  <div className="sub-header">
+      <div className="sub-header">
         <Typography variant="body1" color="primary">
           No Preço <span>(</span>$ $ $ <span> $ $)</span>
         </Typography>
       </div>
+
       {restaurantesNoPreco?.map(restaurante => (
-        <div key={restaurante.id}>
-          <img src={restaurante.imagem} alt={restaurante.nome}/>
-          {restaurante.nome}
-          {restaurante.distancia}
-          {restaurante.nota}
-          {restaurante.tempo_medio}
+        <div className="restaurantesContainer" key={restaurante.id}>
+          <img src={restaurante.imagem} alt={restaurante.nome} />
+          <div className="descricaoContainer">
+            <h3 className="titulo">{restaurante.nome}</h3>
+            {restaurante.distancia}
+            {restaurante.nota}
+            {restaurante.tempo_medio}
+          </div>
         </div>
       ))}
 
-  <div className="sub-header">
+      <div className="sub-header">
         <Typography variant="body1" color="primary">
           Caro mas vale a pena <span>(</span>$ $ $ $ $<span>)</span>
         </Typography>
       </div>
+
       {restaurantesCaro?.map(restaurante => (
-        <div key={restaurante.id}>
-          <img src={restaurante.imagem} alt={restaurante.nome}/>
-          {restaurante.nome}
-          {restaurante.distancia}
-          {restaurante.nota}
-          {restaurante.tempo_medio}
+        <div className="restaurantesContainer" key={restaurante.id}>
+          <img src={restaurante.imagem} alt={restaurante.nome} />
+          <div className="descricaoContainer">
+            <h3 className="titulo">{restaurante.nome}</h3>
+            {restaurante.distancia}
+            {restaurante.nota}
+            {restaurante.tempo_medio}
+          </div>
         </div>
       ))}
-      
+
+
     </Container>
   )
 }
